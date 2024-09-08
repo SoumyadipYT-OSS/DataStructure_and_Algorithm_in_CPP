d521d910139bd4c30ccbb442317a3f7a9854f59a
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-09-08 19:35:25 +0530

    updated non-member overloads of array

--- Ch2.1_Arrays/non-memberOverloads/02_relational_operators.cpp
+++ Ch2.1_Arrays/non-memberOverloads/02_relational_operators.cpp
@@ -8,3 +8,22 @@
 	The less-than comparison (operator<) behaves as if using algorithm lexicographical_compare, which compares the elements sequentially using operator< in a reciprocal manner (i.e., checking both a<b and b<a) and stopping at the first occurrence.
 
 */
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+	array<int, 5> a = { 10,20,30,40,50 };
+	array<int, 5> b = { 10,20,30,40,50 };
+	array<int, 5> c = { 50,40,30,20,10 };
+
+	if (a == b) std::cout << "a and b are equal\n";
+	if (b != c) std::cout << "b and c are not equal\n";
+	if (b < c) std::cout << "b is less than c\n";
+	if (c > b) std::cout << "c is greater than b\n";
+	if (a <= b) std::cout << "a is less than or equal to b\n";
+	if (a >= b) std::cout << "a is greater than or equal to b\n";
+
+	return 0;
+}
