6f8c484a35e0159d04959ac0ea3871bc76d86ebc
SoumyadipYT-OSS <SoumyadipYT-OSS@users.noreply.github.com>
2026-06-17 23:25:28 +0530

    Add new recursion practice problems: Easy and Medium levels

---
+++ Maths_Pattern_Recursion/_10_Medium_Practice.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+#define LIMIT 1000
+
+void fun2(int n) 
+{
+    if (n <= 0)
+        return;
+    if (n > LIMIT)
+        return;
+    cout << n << " ";
+    fun2(2 * n);
+    cout << n << " ";
+}
+
+
+int main() 
+{
+    fun2(100);
+    return 0;
+}
